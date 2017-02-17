a9537e345493d0f47b5dfef1cacf9fad01f83e36
potbunker <gdtmr0ll>
2017-02-17 01:37:47 -0500

    postgresql lock addition

--- reactive/lock.py
+++ reactive/lock.py
@@ -25,3 +25,40 @@ class LockFile(object):
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.release()
+
+
+class PostgresqlLock(object):
+    def __init__(self, lock_domain):
+        from psycopg2.pool import ThreadedConnectionPool
+        import platform
+        import os
+
+        self._lock_detail = (lock_domain, platform.node(), os.getpid())
+        self._session = ThreadedConnectionPool()
+        self._pool = None
+        self._acquired = False
+
+    @property
+    def acquired(self):
+        return self._acquired
+
+    def acquire(self):
+        with self._pool.acquire() as conn:
+            cursor = conn.cursor()
+            timestamp = cursor.callfunc('public.fn_acquire_lock', self._lock_detail)
+        self._acquired = bool(timestamp)
+
+    def release(self):
+        with self._pool.acquire() as conn:
+            cursor = conn.cursor()
+            timestamp = cursor.callfunc('public.fn_release_lock', self._lock_detail)
+        self._acquired = False
+        return self
+
+    def __enter__(self):
+        self.acquire()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.release()
+
